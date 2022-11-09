dc12f14294cfa9029b1fff1195f497b4b8b70d6a
kp <krittayos.kp@gmail.com>
2022-11-10 01:06:16 +0700

    Add | Scene Transition

--- The Typing of the Demon King/Assets/Scripts/System/ChangeScene.cs
+++ The Typing of the Demon King/Assets/Scripts/System/ChangeScene.cs
@@ -3,7 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void OnClick_MoveToScene(string sceneName)
+    public Animator animator;
+
+    private string sceneName;
+
+    public void OnClick_MoveToScene(string scene)
+    {
+        sceneName = scene;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void TransitionComplete()
     {
         SceneManager.LoadScene(sceneName);
     }
