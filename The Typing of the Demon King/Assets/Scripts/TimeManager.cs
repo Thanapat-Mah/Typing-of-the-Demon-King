37db890743fd0f68efc0de982423bcef4807d1fd
kp <krittayos.kp@gmail.com>
2022-12-12 11:28:40 +0700

    Add | Time and Stat manager

---
+++ The Typing of the Demon King/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager Instance;
+    
+    private float _time;
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this) 
+        { 
+            Destroy(this); 
+        } 
+        else 
+        { 
+            Instance = this; 
+        }
+    }
+    
+    private void Start()
+    {
+        enabled = false;
+    }
+    
+    private void Update()
+    {
+        if (enabled)
+        {
+            _time += Time.deltaTime;
+        }
+    }
+    
+    public void StartTimer()
+    {
+        _time = 0;
+        enabled = true;
+    }
+    
+    public void StopTimer()
+    {
+        enabled = false;
+    }
+    
+    public void ContinueTimer()
+    {
+        enabled = true;
+    }
+    
+    public float GetTime()
+    {
+        return _time;
+    }
+}
